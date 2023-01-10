afb2ea8235557e735e31e1aedde1f5808d591bcf
Tom McDonald <tommcdo@gmail.com>
2023-01-10 08:58:54 -0500

    Moar commandz

--- completions.sh
+++ completions.sh
@@ -3,6 +3,9 @@
 zstyle ':completion:*:*:git:*' user-commands \
 	done:'Update references and checkout main branch after this branch was merged upstream' \
 	forked:'Configure upstreams to reflect that this is a fork' \
+	makeup:'Create an upstream remote for a given org' \
 	pr:'Push a new branch to upstream with same name' \
 	rebranch:'Create a branch with local commits since upstream' \
-	remaster:'Update local main branch and rebase this branch onto it'
+	remaster:'Update local main branch and rebase this branch onto it' \
+	review:'Checkout the fork and branch of a pull request' \
+	syncup:'Synchronize a fork with its upstream'
